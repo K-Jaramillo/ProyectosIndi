dabcd970db1f7950f0809eeeac2286a1ba680816
K-Jaramillo <kevinhenaoghk122@gmail.com>
2023-09-15 09:37:05 -0500

    Night Version

--- Python/PingPongWithChatGPT/PingPongWithChatGPT.py
+++ Python/PingPongWithChatGPT/PingPongWithChatGPT.py
@@ -1,201 +1,228 @@
 import pygame
 import sys
 import random
+import math
 
-# Inicialización de pygame
+# Inicialización de Pygame
 pygame.init()
 
-# Definición de colores
-BLANCO = (255, 255, 255)
-NEGRO = (0, 0, 0)
+# Definir dimensiones de la pantalla
+pantalla_ancho = 800
+pantalla_alto = 600
+pantalla = pygame.display.set_mode((pantalla_ancho, pantalla_alto))
+pygame.display.set_caption("Ping Pong Game")
 
-# Configuración de la pantalla
-ANCHO, ALTO = 800, 600
-pantalla = pygame.display.set_mode((ANCHO, ALTO))
-pygame.display.set_caption("Ping Pong")
+# Colores
+color_fondo = (0, 0, 0)
+color_texto = (255, 255, 255)
+color_linea = (255, 255, 255)
+color_raquetas = (255, 255, 255)
+color_pelota = (255, 255, 255)
 
-# Variables del juego
-sensibilidad = 1.0
-velocidad = 5
-jugador1_puntos = 0
-jugador2_puntos = 0
-contador_jugador1 = 0
-contador_jugador2 = 0
-reloj = pygame.time.Clock()
-inversion_colores = False
-inicio = True
-boton_switch_presionado = False  # Declaración de la variable boton_switch_presionado
+# Dimensiones de las raquetas y la pelota
+ancho_raqueta = 10
+alto_raqueta = 100
+ancho_pelota = 10
+alto_pelota = 10
 
-# Raquetas
-raqueta1 = pygame.Rect(50, ALTO // 2 - 70, 10, 140)
-raqueta2 = pygame.Rect(ANCHO - 60, ALTO // 2 - 70, 10, 140)
+# Velocidad de las raquetas y aceleración
+velocidad_raqueta = 5
+aceleracion_raqueta = 1
 
-# Bola
-bola = pygame.Rect(ANCHO // 2 - 15, ALTO // 2 - 15, 30, 30)
-bola_dx = velocidad * random.choice((1, -1))
-bola_dy = velocidad * random.choice((1, -1))
+# Velocidad inicial de la pelota
+angulo = random.uniform(-math.pi / 4, math.pi / 4)  # Ángulo aleatorio entre -45 y 45 grados
+velocidad_pelota = [math.cos(angulo), math.sin(angulo)]  # Velocidad inicial en la dirección del ángulo
 
-# Función para dibujar la pantalla y objetos del juego
-def dibujar():
-    pantalla.fill(BLANCO if not inversion_colores else NEGRO)
-    pygame.draw.rect(pantalla, NEGRO if not inversion_colores else BLANCO, raqueta1)
-    pygame.draw.rect(pantalla, NEGRO if not inversion_colores else BLANCO, raqueta2)
-    pygame.draw.ellipse(pantalla, NEGRO if not inversion_colores else BLANCO, bola)
-    pygame.draw.aaline(pantalla, NEGRO if not inversion_colores else BLANCO, (ANCHO // 2, 0), (ANCHO // 2, ALTO))
+# Magnitud de velocidad constante
+velocidad_constante = 1.0  # Ajusta este valor según tu preferencia
 
-    # Marcador
-    fuente = pygame.font.Font(None, 20)
-    contador_marcador1 = fuente.render(f"Rondas: {contador_jugador1}", True, NEGRO if not inversion_colores else BLANCO)
-    contador_marcador2 = fuente.render(f"Rondas: {contador_jugador2}", True, NEGRO if not inversion_colores else BLANCO)
-    pantalla.blit(contador_marcador1, (ANCHO // 4, 30))
-    pantalla.blit(contador_marcador2, (2.8 * ANCHO // 4, 30))
+# Normalizar el vector de velocidad
+norma = math.sqrt(velocidad_pelota[0] ** 2 + velocidad_pelota[1] ** 2)
+velocidad_pelota[0] = (velocidad_pelota[0] / norma) * velocidad_constante
+velocidad_pelota[1] = (velocidad_pelota[1] / norma) * velocidad_constante
 
-    # Botón de inversión de colores (Switch)
-    switch_color = BLANCO if boton_switch_presionado else NEGRO
-    switch_texto = "Modo Oscuro" if boton_switch_presionado else "Modo Claro"
-    pygame.draw.rect(pantalla, switch_color, (ANCHO - 120, 10, 100, 40))
-    fuente = pygame.font.Font(None, 24)
-    texto_boton = fuente.render(switch_texto, True, BLANCO)
-    pantalla.blit(texto_boton, (ANCHO - 110, 20))
+# Posición inicial de la pelota
+posicion_pelota = [(pantalla_ancho - ancho_pelota) // 2, (pantalla_alto - alto_pelota) // 2]
 
+# Variables de estado del juego
+estado_juego = "inicio"
+puntos_jugador1 = 0
+puntos_jugador2 = 0
+puntos_para_ganar = 11  # Cantidad de puntos para ganar el juego
+
+# Posiciones iniciales de las raquetas y velocidad
+posicion_raqueta_jugador1 = [50, (pantalla_alto - alto_raqueta) // 2]
+posicion_raqueta_jugador2 = [pantalla_ancho - 60, (pantalla_alto - alto_raqueta) // 2]
+velocidad_raqueta_jugador1 = 0
+velocidad_raqueta_jugador2 = 0
+
+# Función para reiniciar la posición de la pelota
+def reiniciar_posicion_pelota():
+    global posicion_pelota, velocidad_pelota
+    # Reinicia la posición de la pelota al centro
+    posicion_pelota = [(pantalla_ancho - ancho_pelota) // 2, (pantalla_alto - alto_pelota) // 2]
+    # Ahora, vamos a decidir aleatoriamente si la pelota se mueve hacia la derecha o hacia la izquierda
+    direccion = random.choice([-1, 1])  # -1 para izquierda, 1 para derecha
+    angulo = random.uniform(-math.pi / 4, math.pi / 4)  # Ángulo aleatorio entre -45 y 45 grados
+    if direccion == 1:
+        velocidad_pelota = [math.cos(angulo), math.sin(angulo)]  # Dirección hacia la derecha
+    else:
+        velocidad_pelota = [-math.cos(angulo), -math.sin(angulo)]  # Dirección hacia la izquierda
+
+# Función para reiniciar el juego
+def reiniciar_juego():
+    global puntos_jugador1, puntos_jugador2, estado_juego
+    puntos_jugador1 = 0
+    puntos_jugador2 = 0
+    reiniciar_posicion_pelota()
+    estado_juego = "inicio"
+
+# Función para la pantalla de inicio
+def pantalla_inicio():
+    global estado_juego
+    # Limpiar pantalla
+    pantalla.fill(color_fondo)
+    # Dibujar texto de inicio
+    font = pygame.font.Font(None, 36)
+    texto1 = font.render("Presiona ESPACIO para empezar", True, color_texto)
+    texto2 = font.render("Presiona ESC para salir", True, color_texto)
+    pantalla.blit(texto1, (200, 250))
+    pantalla.blit(texto2, (250, 300))
     pygame.display.flip()
+    
+    for evento in pygame.event.get():
+        if evento.type == pygame.KEYDOWN:
+            if evento.key == pygame.K_SPACE:
+                estado_juego = "juego"
+            # Verificar si se presionó la tecla ESCAPE para cerrar el programa
+            elif evento.key == pygame.K_ESCAPE:
+                pygame.quit()
+                sys.exit()
 
-# Función para invertir los colores globalmente
-def invertir_colores_globalmente():
-    global inversion_colores
-    inversion_colores = not inversion_colores
 
-# Función para manejar eventos de juego
-def manejar_eventos():
-    global inicio
-    global jugador1_puntos, jugador2_puntos, contador_jugador1, contador_jugador2
-    global bola, bola_dx, bola_dy
-    global boton_switch_presionado  # Declaración de la variable boton_switch_presionado
+# Función para la pantalla de juego
+def pantalla_juego():
+    global estado_juego, puntos_jugador1, puntos_jugador2, posicion_raqueta_jugador1, posicion_raqueta_jugador2, velocidad_raqueta_jugador1, velocidad_raqueta_jugador2, velocidad_pelota, posicion_pelota
+    
+    # Detectar colisión con raqueta del jugador 1
+    if (posicion_pelota[0] <= posicion_raqueta_jugador1[0] + ancho_raqueta and
+        posicion_pelota[1] >= posicion_raqueta_jugador1[1] and
+        posicion_pelota[1] <= posicion_raqueta_jugador1[1] + alto_raqueta):
+            velocidad_pelota[0] = abs(velocidad_pelota[0])  # Cambia la dirección horizontal de la pelota
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+# Detectar colisión con raqueta del jugador 2
+    if (posicion_pelota[0] >= posicion_raqueta_jugador2[0] - ancho_pelota and
+        posicion_pelota[1] >= posicion_raqueta_jugador2[1] and
+        posicion_pelota[1] <= posicion_raqueta_jugador2[1] + alto_raqueta):
+            velocidad_pelota[0] = -abs(velocidad_pelota[0])  # Cambia la dirección horizontal de la pelota
+    # Limpiar pantalla
+    pantalla.fill(color_fondo)
+
+    # Dibujar línea en el centro
+    pygame.draw.line(pantalla, color_linea, (pantalla_ancho // 2, 0), (pantalla_ancho // 2, pantalla_alto), 5)
+
+    # Dibujar raquetas
+    pygame.draw.rect(pantalla, color_raquetas, (posicion_raqueta_jugador1[0], posicion_raqueta_jugador1[1], ancho_raqueta, alto_raqueta))
+    pygame.draw.rect(pantalla, color_raquetas, (posicion_raqueta_jugador2[0], posicion_raqueta_jugador2[1], ancho_raqueta, alto_raqueta))
+
+    # Dibujar pelota
+    pygame.draw.rect(pantalla, color_pelota, (posicion_pelota[0], posicion_pelota[1], ancho_pelota, alto_pelota))
+
+    # Control de eventos
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    
+    # Movimiento de las raquetas
+    keys = pygame.key.get_pressed()
+    
+    # Jugador 1
+    if keys[pygame.K_w] and posicion_raqueta_jugador1[1] > 0:
+        velocidad_raqueta_jugador1 = -velocidad_raqueta
+    elif keys[pygame.K_s] and posicion_raqueta_jugador1[1] < pantalla_alto - alto_raqueta:
+        velocidad_raqueta_jugador1 = velocidad_raqueta
+    else:
+        velocidad_raqueta_jugador1 = 0
+        
+    # Jugador 2
+    if keys[pygame.K_UP] and posicion_raqueta_jugador2[1] > 0:
+        velocidad_raqueta_jugador2 = -velocidad_raqueta
+    elif keys[pygame.K_DOWN] and posicion_raqueta_jugador2[1] < pantalla_alto - alto_raqueta:
+        velocidad_raqueta_jugador2 = velocidad_raqueta
+    else:
+        velocidad_raqueta_jugador2 = 0
 
-        if inicio:
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                inicio = False
-        else:
-            # Movimiento de raquetas
-            teclas = pygame.key.get_pressed()
-            if teclas[pygame.K_w] and raqueta1.top > 0:
-                raqueta1.y -= velocidad
-            if teclas[pygame.K_s] and raqueta1.bottom < ALTO:
-                raqueta1.y += velocidad
-            if teclas[pygame.K_UP] and raqueta2.top > 0:
-                raqueta2.y -= velocidad
-            if teclas[pygame.K_DOWN] and raqueta2.bottom < ALTO:
-                raqueta2.y += velocidad
+    # Aplicar velocidad a las raquetas
+    posicion_raqueta_jugador1[1] += velocidad_raqueta_jugador1
+    posicion_raqueta_jugador2[1] += velocidad_raqueta_jugador2
 
-            # Movimiento de la bola
-            bola.x += bola_dx
-            bola.y += bola_dy
+    # Movimiento de la pelota (velocidad constante)
+    posicion_pelota[0] += velocidad_pelota[0]
+    posicion_pelota[1] += velocidad_pelota[1]
 
-            # Colisiones con las raquetas
-            if bola.colliderect(raqueta1) or bola.colliderect(raqueta2):
-                bola_dx *= -1
+    # Colisión con las paredes superior e inferior
+    if posicion_pelota[1] <= 0 or posicion_pelota[1] >= pantalla_alto - alto_pelota:
+        velocidad_pelota[1] = -velocidad_pelota[1]
 
-            # Puntuación
-            if bola.left <= 0:
-                jugador2_puntos += 1
-                if jugador2_puntos == 7:
-                    contador_jugador2 += 1
-                    jugador2_puntos = 0
-                    if contador_jugador2 == 7:
-                        pantalla_reinicio(2)
-                    else:
-                        bola = pygame.Rect(ANCHO // 2 - 15, ALTO // 2 - 15, 30, 30)
-                        bola_dx = velocidad * random.choice((1, -1))
-                        bola_dy = velocidad * random.choice((1, -1))
-            if bola.right >= ANCHO:
-                jugador1_puntos += 1
-                if jugador1_puntos == 7:
-                    contador_jugador1 += 1
-                    jugador1_puntos = 0
-                    if contador_jugador1 == 7:
-                        pantalla_reinicio(1)
-                    else:
-                        bola = pygame.Rect(ANCHO // 2 - 15, ALTO // 2 - 15, 30, 30)
-                        bola_dx = velocidad * random.choice((1, -1))
-                        bola_dy = velocidad * random.choice((1, -1))
+    # Colisión con las paredes izquierda y derecha
+    if posicion_pelota[0] <= 0 or posicion_pelota[0] >= pantalla_ancho - ancho_pelota:
+        velocidad_pelota[0] = -velocidad_pelota[0]
 
-            # Colisiones con los bordes
-            if bola.top <= 0 or bola.bottom >= ALTO:
-                bola_dy *= -1
+    # Verificar si la pelota ha llegado al borde izquierdo (puntos para jugador 2)
+    if posicion_pelota[0] <= 0:
+        puntos_jugador2 += 1
+        reiniciar_posicion_pelota()
+    # Verificar si la pelota ha llegado al borde derecho (puntos para jugador 1)
+    if posicion_pelota[0] >= pantalla_ancho - ancho_pelota:
+        puntos_jugador1 += 1
+        reiniciar_posicion_pelota()
 
-            # Manejar el clic del botón "Switch"
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    x, y = event.pos
-                    if ANCHO - 120 <= x <= ANCHO - 20 and 10 <= y <= 50:
-                        boton_switch_presionado = not boton_switch_presionado
-                        invertir_colores_globalmente()
-                        dibujar()
+    # Verificar fin del juego y reiniciar
+    if puntos_jugador1 >= puntos_para_ganar or puntos_jugador2 >= puntos_para_ganar:
+        reiniciar_juego()
 
-# Pantalla de inicio
-def pantalla_inicio():
-    global inicio
-    pantalla.fill(BLANCO if not inversion_colores else NEGRO)
-    fuente = pygame.font.Font(None, 30)
-    texto = fuente.render("Presiona ESPACIO para comenzar", True, NEGRO if not inversion_colores else BLANCO)
-    pantalla.blit(texto, (ANCHO // 4, ALTO // 2))
-    pantalla.fill(BLANCO if not inversion_colores else NEGRO)
+    # Actualizar marcador
+    font = pygame.font.Font(None, 36)
+    texto_puntos = font.render(f"Jugador 1: {puntos_jugador1}  Jugador 2: {puntos_jugador2}", True, color_texto)
+    pantalla.blit(texto_puntos, (280, 20))
+
     pygame.display.flip()
-    esperando_inicio = True
-    while esperando_inicio:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                inicio = False
-                esperando_inicio = False
 
-# Pantalla de reinicio
-def pantalla_reinicio(ganador):
-    global inicio
-    pantalla.fill(BLANCO if not inversion_colores else NEGRO)
-    fuente = pygame.font.Font(None, 30)
-    texto = fuente.render(f"Jugador {ganador} ha ganado 3 rondas.", True, NEGRO if not inversion_colores else BLANCO)
-    pantalla.blit(texto, (ANCHO // 4, ALTO // 2 - 30))
-    texto2 = fuente.render("Presiona ESPACIO para reiniciar", True, NEGRO if not inversion_colores else BLANCO)
-    pantalla.blit(texto2, (ANCHO // 4, ALTO // 2 + 10))
-    pygame.draw.rect(pantalla, NEGRO, (ANCHO - 120, 10, 100, 40))
-    fuente = pygame.font.Font(None, 24)
-    texto_boton = fuente.render("Switch", True, BLANCO)
-    pantalla.blit(texto_boton, (ANCHO - 110, 20))
-    pantalla.fill(BLANCO if not inversion_colores else NEGRO)
+# Función para la pantalla de fin de juego
+def pantalla_fin():
+    global estado_juego
+    # Limpiar pantalla
+    pantalla.fill(color_fondo)
+    # Determinar al ganador
+    if puntos_jugador1 > puntos_jugador2:
+        ganador = "Jugador 1"
+    else:
+        ganador = "Jugador 2"
+    # Mostrar resultado
+    font = pygame.font.Font(None, 48)
+    texto = font.render(f"{ganador} gana!", True, color_texto)
+    pantalla.blit(texto, (300, 250))
+    texto2 = font.render("Presiona ESPACIO para jugar de nuevo", True, color_texto)
+    pantalla.blit(texto2, (200, 350))
     pygame.display.flip()
-    esperando_reinicio = True
-    while esperando_reinicio:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                global jugador1_puntos, jugador2_puntos, contador_jugador1, contador_jugador2
-                jugador1_puntos = 0
-                jugador2_puntos = 0
-                contador_jugador1 = 0
-                contador_jugador2 = 0
-                inicio = True  # Asegúrate de establecer 'inicio' en True
-                esperando_reinicio = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    x, y = event.pos
-                    if ANCHO - 120 <= x <= ANCHO - 20 and 10 <= y <= 50:
-                        invertir_colores_globalmente()  # Invertir colores globalmente al hacer clic en el botón en la pantalla de reinicio
-                        pantalla_inicio()  # Llamar a pantalla_inicio() cuando se haga clic en el botón
+
+    for evento in pygame.event.get():
+        if evento.type == pygame.KEYDOWN:
+            if evento.key == pygame.K_SPACE:
+                reiniciar_juego()
+                estado_juego = "juego"
 
 # Bucle principal del juego
-pantalla_inicio()  # Espera a que el jugador presione ESPACIO para iniciar
 while True:
-    manejar_eventos()
-    dibujar()
-    reloj.tick(60)
-
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    
+    if estado_juego == "inicio":
+        pantalla_inicio()
+    elif estado_juego == "juego":
+        pantalla_juego()
+    elif estado_juego == "fin":
+        pantalla_fin()
